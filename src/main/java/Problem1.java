e3709e7d1a118722801d11f981c8472c5cae2ce6
Crabs-hub <72178189+Crabs-hub@users.noreply.github.com>
2020-10-08 23:41:29 -0700

    Update Problem1.java

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -3,10 +3,20 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
-        for (int i = 0 ; i < data.length ; i++){
-            if(data[i] == target)
-            {
-                return i;
+        int low = 0;
+        int high = data.length - 1;
+
+
+        while (low <= high){
+            int mid = (low + high)/2;
+            if(target > data[mid]){
+                low = mid + 1;
+            }
+            else if (target < data[mid]){
+                high = mid - 1;
+            }
+            else if ( target == data[mid]){
+                return mid;
             }
         }
         return -1; // Place holder
